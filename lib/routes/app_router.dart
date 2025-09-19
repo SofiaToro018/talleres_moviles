9d4c4b6c6dd938ea167b848c083e27c2451c5468
Sofia Toro <laura.toro01@uceva.edu.co>
2025-09-19 15:09:40 -0500

    feat: Eliminar ruta de PasoParametros y ajustar rutas en app_router

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,17 +1,13 @@
 import 'package:go_router/go_router.dart';
 import '../widgets/tabbar.dart';
-import '../view/paso_parametros/paso_parametros_screen.dart';
 import '../view/paso_parametros/detalle_screen.dart';
 import '../view/ciclo_vida/ciclo_vida_screen.dart';
+import '../view/home/home_screen.dart';
 
 final GoRouter appRouter = GoRouter(
   routes: [
     GoRoute(path: '/', builder: (context, state) => const TabBarWidget()),
-    GoRoute(
-      path: '/paso_parametros',
-      name: 'paso_parametros',
-      builder: (context, state) => const PasoParametrosScreen(),
-    ),
+    GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
     GoRoute(
       path: '/detalle/:parametro/:metodo',
       builder: (context, state) {
