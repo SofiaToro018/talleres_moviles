408ea2d6c37faf7684be7d0009f8f8972a00fa60
Sofia Toro <laura.toro01@uceva.edu.co>
2025-10-03 17:36:47 -0500

    feat: refactor HomeScreen y  BaseView, integrando CustomDrawer para mejorar la navegación

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -6,19 +6,46 @@ import '../view/home/home_screen.dart';
 
 final GoRouter appRouter = GoRouter(
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const TabBarWidget()),
-    GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
     GoRoute(
-      path: '/detalle/:parametro/:metodo',
+      path: '/',
+      builder: (context, state) => const HomeScreen(), // Usa HomeView
+    ),
+    // Rutas para el paso de parámetros
+    GoRoute(
+      path: '/paso_parametros',
+      name: 'paso_parametros',
+      builder: (context, state) => const PasoParametrosScreen(),
+    ),
+
+    // !Ruta para el detalle con parámetros
+    GoRoute(
+      path:
+          '/detalle/:parametro/:metodo', //la ruta recibe dos parametros los " : " indican que son parametros
       builder: (context, state) {
+        //*se capturan los parametros recibidos
+        // declarando las variables parametro y metodo
+        // es final porque no se van a modificar
         final parametro = state.pathParameters['parametro']!;
         final metodo = state.pathParameters['metodo']!;
         return DetalleScreen(parametro: parametro, metodoNavegacion: metodo);
       },
     ),
+    //!Ruta para el ciclo de vida
     GoRoute(
       path: '/ciclo_vida',
       builder: (context, state) => const CicloVidaScreen(),
+    ),
+    //!Ruta para FUTURE
+    GoRoute(
+      path: '/future',
+      name: 'future',
+      builder: (context, state) => const FutureView(),
+    ),
+    //!Ruta para ISOLATE
+    GoRoute(
+      path: '/isolate',
+      name: 'isolate',
+      builder: (context, state) => const IsolateView(),
     ),
   ],
 );
--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,83 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../themes/app_theme.dart';
+import '../../widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Ítems que enviarán un parámetro y usarán distintos métodos de navegación
-    final List<Map<String, String>> items = [
-      {'label': 'Navegar con Go', 'param': 'Item 1', 'method': 'go'},
-      {'label': 'Navegar con Push', 'param': 'Item 2', 'method': 'push'},
-      {'label': 'Navegar con Replace', 'param': 'Item 3', 'method': 'replace'},
-    ];
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      appBar: AppBar(title: const Text('Taller Segundo Plano')),
+      drawer: const CustomDrawer(),
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Bienvenido al Dashboard del Taller 2',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/future'),
+              child: const Text("Future / async-await"),
             ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: GridView.builder(
-                itemCount: items.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 1.2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                ),
-                itemBuilder: (context, index) {
-                  final item = items[index];
-                  final param = item['param']!;
-                  final method = item['method']!;
-                  return GestureDetector(
-                    onTap: () {
-                      // Selecciona método de navegación según el item
-                      if (method == 'go') {
-                        context.go('/detalle/$param/$method');
-                      } else if (method == 'push') {
-                        context.push('/detalle/$param/$method');
-                      } else {
-                        context.replace('/detalle/$param/$method');
-                      }
-                    },
-                    child: Card(
-                      color: AppTheme.cardColor,
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Hero(
-                        tag:
-                            param, // 👈 Hero usa el parámetro como identificador único
-                        child: Card(
-                          color: const Color.fromARGB(255, 142, 97, 225),
-                          elevation: 4,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Center(
-                            child: Text(
-                              item['label']!,
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/timer'),
+              child: const Text("Cronómetro"),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/isolate'),
+              child: const Text("Isolate Tarea Pesada"),
             ),
           ],
         ),
